cbade5b8c27ac98a92bf5730b22c33fddce85f60
Michael Kerres <m.kerres@gmx.net>
2021-11-28 08:12:33 +0100

    Thread hinzugefügt, der die Performance mit ProcessTimer.cs ausgibt.

--- Performance/Performance/MainWindow.xaml.cs
+++ Performance/Performance/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,21 @@ namespace Performance
     /// </summary>
     public partial class MainWindow : Window
     {
+        Thread newThread;
+
+        ProcessTimer w;
+
         public MainWindow()
         {
             InitializeComponent();
+            w = new ProcessTimer();
+
+            StartThread();
+        }
+        private void StartThread()
+        {
+            newThread = new Thread(w.DoWork);
+            newThread.Start();
         }
     }
 }
---
+++ Performance/Performance/ProcessTimer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace Performance
+{
+    internal class ProcessTimer
+    {
+        PerformanceCounter pRam;
+        PerformanceCounter pCpu;
+
+        public ProcessTimer()
+        {
+            //pCpu = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+            InitialisierePerformanceCounter();
+        }
+
+        void InitialisierePerformanceCounter() // Initialisieren
+        {
+            pCpu = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            pRam = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+            PerformanceCounterCategory[] categories;
+            categories = PerformanceCounterCategory.GetCategories();
+
+            //    cpuCounter.CategoryName = "Processor";
+            //    cpuCounter.CounterName = "% Processor Time";
+            //    cpuCounter.InstanceName = "_Total"; // "_Total" entspricht der gesamten CPU Auslastung, Bei Computern mit mehr als 1 logischem Prozessor: "0" dem ersten Core, "1" dem zweiten...
+        }
+
+        internal void DoWork(object obj)
+        {
+            while (true)
+            {
+                GetCPUusage();
+                Thread.Sleep(1000);
+            }
+        }
+
+        float GetCPUusage() // Liefert die aktuelle Auslastung zurück
+        {
+            float retVal1 = pCpu.NextValue();
+            float retVal2 = pRam.NextValue();
+
+            return retVal1;
+        }
+
+    }
+}
